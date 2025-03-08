fdbcc767528f39656904adb0c845da27ee306817
Papa Abdoulaye Ndiaye <abdoulayendiaye10000@gmail.com>
2025-03-08 13:06:22 +0000

    feat: generate component for task and project Add logic to retrieve all tasks and projects.

--- angularFront/src/app/app.component.ts
+++ angularFront/src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ApiService } from './api.service';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { ProjectComponent } from './project/project.component';
+import { TaskComponent } from './task/task.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, HttpClientModule, ProjectComponent, TaskComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: [ApiService]
 })
 export class AppComponent {
   title = 'angularFront';
