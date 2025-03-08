72d141a902e2c1c4814ed1f6b47d81e4dd5a2d5a
Papa Abdoulaye Ndiaye <abdoulayendiaye10000@gmail.com>
2025-03-08 13:33:31 +0000

    fix: update serialized fields for user model

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -5,4 +5,10 @@ from authentication.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = "__all__"
+        fields = ('id', 'username', 'email', 'password', 'is_teacher', 'is_student', 'avatar')
+        extra_kwargs = {'password': {'write_only': True, 'required': True}}
+
+        def create(self, validated_data):
+            # Make sure that password passing through HTTP to be hashed
+            user = User.objects.create_user(**validated_data)
+            return user
