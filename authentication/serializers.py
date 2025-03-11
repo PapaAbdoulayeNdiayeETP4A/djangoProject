ba77d5cea2280ecd744708ecb3f6fe9355cf17d7
Papa Abdoulaye Ndiaye <abdoulayendiaye10000@gmail.com>
2025-03-11 15:31:33 +0000

    fix: adjust models and modify serializers to include relationnal models

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,11 +1,14 @@
 from rest_framework import serializers
 from authentication.models import User
+from gtachesapp.serializers import ProjectSerializer
 
 
 class UserSerializer(serializers.ModelSerializer):
+    projects = ProjectSerializer(many=True)
+
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'password', 'is_teacher', 'is_student', 'avatar')
+        fields = ('id', 'username', 'email', 'password', 'is_teacher', 'is_student', 'avatar', 'projects')
         extra_kwargs = {'password': {'write_only': True, 'required': True}}
 
         def create(self, validated_data):
--- gtachesapp/serializers.py
+++ gtachesapp/serializers.py
@@ -1,15 +1,16 @@
 from rest_framework import serializers
-
 from gtachesapp.models import Project, Task
-
-
-class ProjectSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Project
-        fields = "__all__"
 
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = "__all__"
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    tasks = TaskSerializer(many=True)
+
+    class Meta:
+        model = Project
+        fields = ('id', 'name', 'description', 'owner', 'tasks')
