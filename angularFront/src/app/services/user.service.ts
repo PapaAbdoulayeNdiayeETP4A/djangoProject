d41c61e32a0ad1d95ec06679924d856c2509b564
Papa Abdoulaye Ndiaye <abdoulayendiaye10000@gmail.com>
2025-03-13 18:01:02 +0000

    feat: better user authentication handling Generate access and refresh token using JWT of django rest framework on user loging. Add token_blacklist app and run migrations: thats allow us to refresh tokens using the refresh token.

--- angularFront/src/app/services/user.service.ts
+++ angularFront/src/app/services/user.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { Observable, tap, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -17,7 +17,7 @@ export class UserService {
   }
 
   loginUser(userData: any): Observable<any> {
-    return this.http.post<{access: string, refresh: string}>(this.loginUrl, userData).pipe(
+    return this.http.post<{ access: string, refresh: string }>(this.loginUrl, userData).pipe(
       tap(response => {
         localStorage.setItem('access_token', response.access);
         localStorage.setItem('refresh_token', response.refresh);
@@ -26,11 +26,34 @@ export class UserService {
   }
 
   logoutUser() {
-    localStorage.removeItem('access_token');
-    localStorage.removeItem('refresh_token');
+    return this.http.get('http://127.0.0.1:8000/api/project/');
   }
+
 
   isAuthenticated(): boolean {
     return !!localStorage.getItem('access_token');
   }
+
+  refreshToken(): Observable<any> {
+    const refreshToken = localStorage.getItem('refresh_token');
+
+    if (!refreshToken) {
+      return throwError(() => new Error('Pas de refresh token disponible'));
+    }
+
+    return this.http.post<{ access: string, refresh: string }>(
+      'http://127.0.0.1:8000/api/token/refresh/',
+      { refresh: refreshToken }
+    ).pipe(
+      tap(response => {
+        if (response && response.access) {
+          localStorage.setItem('access_token', response.access);
+          if (response.refresh) {
+            localStorage.setItem('refresh_token', response.refresh);
+          }
+        }
+      })
+    );
+  }
+
 }
